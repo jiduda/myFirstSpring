63f1bcd76341fab9e5a8f9e9fc3c0fb49b458c04
Jiduda <rlawldnjs2000@gmail.com>
2023-05-16 11:59:21 +0900

    done_member

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,46 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     */
+    public long join(Member member){
+        //중복된 이름이 있을 경우 제외
+        validateDuplicatedMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("중복된 이름이 있습니다.");
+                        });
+    }
+
+    /**
+     * 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> fineOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,74 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+
+    }
+
+    @AfterEach  // 메서드 하나가 끝날 때마다 실행
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("Jiduda");
+        // when
+        Long memberId = memberService.join(member);
+        // then
+        Member findMember = memberService.fineOne(memberId).get();
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        // given
+        Member member1 = new Member();
+        member1.setName("same");
+
+        Member member2 = new Member();
+        member2.setName("same");
+        // when
+        Long memberId1 = memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("중복된 이름이 있습니다.");
+        /*try {
+            Long memberId2 = memberService.join(member2);
+            fail("예외가 발생해야 합니다.");
+        } catch(IllegalStateException e){
+            assertThat(e.getMessage()).isEqualTo("중복된 이름이 있습니다.");
+        }*/
+        // then
+    }
+
+    @Test
+    void 전체회원리스트() {
+    }
+
+    @Test
+    void 회원찾기() {
+    }
+}
